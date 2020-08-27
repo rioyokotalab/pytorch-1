076af03ff24b2e56702d0b18f88657816c4fc3a1
Akao, Kazutoshi <akao.kazutoshi@jp.fujitsu.com>
2020-08-27 14:41:00 +0900

    refactor: Remove old code.

--- aten/src/ATen/native/fujitsu/Random.cpp
+++ aten/src/ATen/native/fujitsu/Random.cpp
@@ -36,8 +36,8 @@
 namespace at {
 namespace native {
 
-Tensor& _bernoulli_fujitsu_(Tensor& self, double p, c10::optional<Generator> gen) {
-  AT_ERROR("bernoulli_fujitsu_kernel: This CPU is not supported yet");
+void _bernoulli_fujitsu_(Tensor& self, double p, c10::optional<Generator> gen) {
+  AT_ERROR("_bernoulli_fujitsu_: ATen not compiled with AIL support");
 }
 
 } // namespace native
@@ -66,7 +66,7 @@ Tensor& _bernoulli_fujitsu_(Tensor& self, double p, c10::optional<Generator> gen
 namespace at {
 namespace native {
 
-Tensor& _bernoulli_fujitsu_(Tensor& self, double p, c10::optional<Generator> gen) {
+void _bernoulli_fujitsu_(Tensor& self, double p, c10::optional<Generator> gen) {
   CPUGeneratorImpl* generator = get_generator_or_default<CPUGeneratorImpl>(gen, detail::getDefaultCPUGenerator());
   int64_t seed;
   {
@@ -110,54 +110,6 @@ Tensor& _bernoulli_fujitsu_(Tensor& self, double p, c10::optional<Generator> gen
       self.copy_(tmp_int_tensor);
     }
   });
-
-  return self;
-  /*
-  CPUGenerator* generator = get_generator_or_default<CPUGenerator>(gen, detail::getDefaultCPUGenerator());
-  int64_t seed;
-  {
-    // See Note [Acquire lock when using random generators]
-    std::lock_guard<std::mutex> lock(generator->mutex_);
-    seed = generator->random();
-  }
-  int64_t n = self.numel();
-  bool contig = self.is_contiguous();
-
-  AT_DISPATCH_ALL_TYPES_AND(at::ScalarType::Bool, self.scalar_type(), "_bernoulli_fujitsu_", [&] {
-    at::Tensor tmp_int_tensor;
-    if (std::is_same<scalar_t, int>::value && contig) {
-      tmp_int_tensor = self;
-    } else {
-      tmp_int_tensor = at::empty(self.sizes(), self.options().dtype(at::kInt));
-    }
-
-    scalar_t *self_ptr = self.data_ptr<scalar_t>();
-    int *sample_int_ptr = tmp_int_tensor.data_ptr<int>();
-
-    auto sample = [&](int64_t begin, int64_t end) {
-      int64_t len = end - begin;
-      if (len > 0) {
-        RngBernoulli_fujitsu(0, seed, len, sample_int_ptr + begin, p);
-
-        // vectorized copy if using buffer and contiguous, i.e., being non-int
-        // type and contiguous
-        if (!std::is_same<scalar_t, int>::value && contig) {
-          scalar_t *self_seg = self_ptr + begin;
-          int* tmp_seg = sample_int_ptr + begin;
-          at::vec256::convert<int, scalar_t>(tmp_seg, self_seg, len);
-        }
-      }
-    };
-*/
-//    parallel_for(0, n, /* grain_size= */ 800, sample);
-/*
-    // copy_ if using buffer and non contiguous
-    if (!contig) {
-      self.copy_(tmp_int_tensor);
-    }
-  });
-  return self;
-*/
 }
 
 }  // namespace native
