b77cd73e76f7b2c7a2c3f0d9fee3b9fd37c65829
Takashi Sawada <tsawada@fujitsu.com>
2020-11-05 18:15:46 +0900

    fix: [v1.7.0] Fix ASIS binary ops bug.

--- aten/src/ATen/native/BinaryOps.cpp
+++ aten/src/ATen/native/BinaryOps.cpp
@@ -56,6 +56,9 @@ static Tensor wrapped_scalar_tensor(Scalar scalar) {
 }
 
 Tensor& add_out(Tensor& result, const Tensor& self, const Tensor& other, Scalar alpha) {
+  if (self.is_mkldnn()) {
+    return native::mkldnn_add_out(result, self, other, alpha);
+  }
   auto iter = TensorIterator::binary_op(result, self, other);
   alpha_check(iter.dtype(), alpha);
   add_stub(iter.device_type(), iter, alpha);
@@ -241,6 +244,9 @@ Tensor& floor_divide_(Tensor& self, const Tensor& other) {
 }
 
 Tensor& mul_out(Tensor& result, const Tensor& self, const Tensor& other) {
+  if (self.is_mkldnn()) {
+    return native::mkldnn_mul_out(result, self, other);
+  }
   auto iter = TensorIterator::binary_op(result, self, other);
   mul_stub(iter.device_type(), iter);
   return result;
